960f4b8d3460887bae77448bb530f4e0efa8bdc3
irarubrian <brian.o.iraru@gmail.com>
2024-12-05 14:28:20 +0300

    object lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// Write your solution here
+
+const employee = {
+    name:"sam",
+    streetAddress:"11 Broadway"
+}
+
+
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmployee = {...employee};
+    newEmployee[key]=value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value;
+    return employee;
+} 
+function deleteFromEmployeeByKey(employee,key,value){
+    const newEmployee  = {...employee};
+    newEmployee[key]=value;
+    return newEmployee;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key,value){
+    employee[key]=value;
+    return employee;
+}
